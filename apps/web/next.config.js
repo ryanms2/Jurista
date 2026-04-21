99dda1d30665be1b9e0549bf3954ef2aaccc0ead
Rian Santos <riansantos355@gmail.com>
2026-04-21 13:00:39 -0300

    Fix Prisma output path

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -7,18 +7,6 @@ const nextConfig = {
       bodySizeLimit: "10mb",
     },
   },
-  // Needed for Prisma
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        tls: false,
-      };
-    }
-    return config;
-  },
 };
 
 module.exports = nextConfig;
